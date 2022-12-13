2b5d6c5a0216da1ab7bbe3fc58eedee7d17be697
Rokas <rookas.rudzenskas@gmail.com>
2022-12-13 11:37:17 +0200

    Add a canvas and render the scene
    This commit adds a canvas to the page and renders the scene to it.

--- main.jsx
+++ main.jsx
@@ -13,4 +13,12 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 // Add a camera
-const camera = new THREE.PerspectiveCamera(45, )
+const camera = new THREE.PerspectiveCamera(45, 800, 600);
+scene.add(camera);
+
+// Render the scene
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
